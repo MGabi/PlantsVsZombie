4bc9a0ddb4826bb605a365239c728d03c856cce5
Gabi M <ytgabi98@gmail.com>
2017-04-19 16:00:07 +0300

    main menu designed

---
+++ Assets/Scripts/OpenSceneScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class OpenSceneScript : MonoBehaviour {
+	public string sceneName;
+
+	void Start () {
+		Application.LoadLevel (sceneName);
+	}
+}
