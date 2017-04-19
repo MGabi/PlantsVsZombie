acce1bab4bddc7118cd4c54114f44cf5bcced976
xdnorbii <xd.norbii@gmail.com>
2017-04-19 16:45:35 +0300

    zombie added

---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour {
+
+    public int health;
+    public bool isEnemy;
+
+	public void DoDamage(int dmg)
+    {
+        health -= dmg;
+        if (health <= 0)
+            Destroy(this.gameObject);
+    }
+}
