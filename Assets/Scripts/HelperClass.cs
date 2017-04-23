54add429fcfdadca8e9137981037ce5c2fb39b9b
Gabi M <ytgabi98@gmail.com>
2017-04-23 14:08:20 +0300

    plant levels added

--- Assets/Scripts/HelperClass.cs
+++ Assets/Scripts/HelperClass.cs
@@ -15,6 +15,8 @@ public class HelperClass : MonoBehaviour
 	public static bool FLOWER_FREEZE = false;
 	public static bool FLOWER_EXPLODE = true;
 
+	// Plants properties
+
 	public static string PREF_NAME = "ID_NAME";
 	public static string PREF_FINALSCORE = "ID_FINALSCORE";
 	public static string PREF_BALANCE = "ID_BALANCE";
@@ -24,6 +26,31 @@ public class HelperClass : MonoBehaviour
 	public static string PREF_F_WALL = "ID_F_WALL";
 	public static string PREF_F_FREEZE = "ID_F_FREEZE";
 	public static string PREF_F_EXPLODE = "ID_F_EXPLODE";
+
+	public static string PREF_LVL_SUN = "ID_LVL_SUN";
+	public static string PREF_LVL_SHOOTER = "ID_LVL_SHOOTER";
+	public static string PREF_LVL_BOMB = "ID_LVL_BOMB";
+	public static string PREF_LVL_WALL = "ID_LVL_WALL";
+	public static string PREF_LVL_FREEZE = "ID_LVL_FREEZE";
+	public static string PREF_LVL_EXPLODE = "ID_LVL_FREEZE";
+
+	public static string PREF_HEALTH_SUN = "ID_HEALTH_SUN";
+	public static string PREF_HEALTH_SHOOTER = "ID_HEALTH_SUN";
+	public static string PREF_HEALTH_WALL = "ID_HEALTH_WALL";
+	public static string PREF_HEALTH_FREEZE = "ID_HEALTH_FREEZE";
+
+	public static string PREF_DMG_SHOOTER = "ID_DMG_SHOOTER";
+	public static string PREF_DMG_BOMB = "ID_DMG_BOMB";
+	public static string PREF_DMG_FREEZE = "ID_DMG_FREEZE";
+	public static string PREF_DMG_EXPLODE = "ID_DMG_EXPLODE";
+
+	public static string PREF_SPEED_SHOOTER = "ID_SPEED_SHOOTER";
+	public static string PREF_SPEED_FREEZE = "ID_SPEED_FREEZE";
+
+	public static string PREF_SUN_COOLDOWN = "ID_SUN_COOLDOWN";
+	public static string PREF_FREEZE_SLOWMO = "ID_SUN_SLOWMO";
+
+
 
 	void Awake()
 	{
@@ -38,7 +65,7 @@ public class HelperClass : MonoBehaviour
 
 	public void initData()
 	{
-		PlayerPrefs.DeleteKey(PREF_BALANCE);
+		//PlayerPrefs.DeleteKey(PREF_BALANCE);
 		if (!PlayerPrefs.HasKey(PREF_NAME))
 			PlayerPrefs.SetString(PREF_NAME, "Name");
 
@@ -48,6 +75,7 @@ public class HelperClass : MonoBehaviour
 		if (!PlayerPrefs.HasKey(PREF_BALANCE))
 			PlayerPrefs.SetString(PREF_BALANCE, "50000");
 
+		//Available plants
 		if (!PlayerPrefs.HasKey(PREF_F_SUN))
 			PlayerPrefs.SetString(PREF_F_SUN, "true");
 
@@ -65,6 +93,62 @@ public class HelperClass : MonoBehaviour
 
 		if (!PlayerPrefs.HasKey(PREF_F_EXPLODE))
 			PlayerPrefs.SetString(PREF_F_EXPLODE, "false");
+
+		//Level
+		if (!PlayerPrefs.HasKey(PREF_LVL_SUN))
+			PlayerPrefs.SetString(PREF_LVL_SUN, "1");
+
+		if (!PlayerPrefs.HasKey(PREF_LVL_SHOOTER))
+			PlayerPrefs.SetString(PREF_LVL_SHOOTER, "1");
+
+		if (!PlayerPrefs.HasKey(PREF_LVL_BOMB))
+			PlayerPrefs.SetString(PREF_LVL_BOMB, "1");
+
+		if (!PlayerPrefs.HasKey(PREF_LVL_WALL))
+			PlayerPrefs.SetString(PREF_LVL_WALL, "1");
+
+		if (!PlayerPrefs.HasKey(PREF_LVL_FREEZE))
+			PlayerPrefs.SetString(PREF_LVL_FREEZE, "1");
+
+		if (!PlayerPrefs.HasKey(PREF_LVL_EXPLODE))
+			PlayerPrefs.SetString(PREF_LVL_EXPLODE, "1");
+
+		//Health
+		if (!PlayerPrefs.HasKey(PREF_HEALTH_SUN))
+			PlayerPrefs.SetString(PREF_HEALTH_SUN, PlantAttrs.SUN_1_HEALTH);
+
+		if (!PlayerPrefs.HasKey(PREF_HEALTH_SHOOTER))
+			PlayerPrefs.SetString(PREF_HEALTH_SHOOTER, PlantAttrs.SHOOTER_1_HEALTH);
+
+		if (!PlayerPrefs.HasKey(PREF_HEALTH_WALL))
+			PlayerPrefs.SetString(PREF_HEALTH_WALL, PlantAttrs.WALLNUT_1_HEALTH);
+
+		if (!PlayerPrefs.HasKey(PREF_HEALTH_FREEZE))
+			PlayerPrefs.SetString(PREF_HEALTH_FREEZE, PlantAttrs.FREEZE_1_HEALTH);
+
+		//Damage
+		if (!PlayerPrefs.HasKey(PREF_DMG_SHOOTER))
+			PlayerPrefs.SetString(PREF_DMG_SHOOTER, PlantAttrs.SHOOTER_1_DMG);
+
+		if (!PlayerPrefs.HasKey(PREF_DMG_BOMB))
+			PlayerPrefs.SetString(PREF_DMG_BOMB, PlantAttrs.BOMB_1_DMG);
+
+		if (!PlayerPrefs.HasKey(PREF_DMG_FREEZE))
+			PlayerPrefs.SetString(PREF_DMG_FREEZE, PlantAttrs.FREEZE_1_DMG);
+
+		//Speed
+		if (!PlayerPrefs.HasKey(PREF_SPEED_SHOOTER))
+			PlayerPrefs.SetString(PREF_SPEED_SHOOTER, PlantAttrs.SHOOTER_1_SPEED);
+
+		if (!PlayerPrefs.HasKey(PREF_SPEED_FREEZE))
+			PlayerPrefs.SetString(PREF_SPEED_FREEZE, PlantAttrs.FREEZE_1_SPEED);
+
+		//Others
+		if (!PlayerPrefs.HasKey(PREF_SUN_COOLDOWN))
+			PlayerPrefs.SetString(PREF_SUN_COOLDOWN, PlantAttrs.SUN_1_COOLDOWN);
+
+		if (!PlayerPrefs.HasKey(PREF_FREEZE_SLOWMO))
+			PlayerPrefs.SetString(PREF_FREEZE_SLOWMO, PlantAttrs.FREEZE_1_SLOWMO);
 
 		PlayerPrefs.Save();
 	}
---
+++ Assets/Scripts/PlantAttrs.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantAttrs : MonoBehaviour
+{
+
+	public static string SUN_1_HEALTH = "1";
+	public static string SUN_1_COOLDOWN = "1";
+
+	public static string SHOOTER_1_HEALTH = "1";
+	public static string SHOOTER_1_DMG = "1";
+	public static string SHOOTER_1_SPEED = "1";
+
+	public static string BOMB_1_DMG = "1";
+
+	public static string WALLNUT_1_HEALTH = "1";
+
+	public static string FREEZE_1_HEALTH = "1";
+	public static string FREEZE_1_DMG = "1";
+	public static string FREEZE_1_SPEED = "1";
+	public static string FREEZE_1_SLOWMO = "1";
+
+
+	/**
+	 * TYPE = 1 -> pe coloana
+	 * TYPE = 2 -> pe linie
+	 * TYPE = 3 -> pe o anumita raza
+	 */
+	public static string EXPLODE_TYPE = "1";
+	public static string EXPLODE_RADIUS = "1";
+}
